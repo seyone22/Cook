797dc2ac67592c8b5eab45811b8a905de0fa6573
Seyone Gunasingham <s.g.seyone@protonmail.com>
2024-06-07 01:19:52 +0530

    Added function to get cost of unavailable items

--- app/src/main/java/com/seyone22/cook/helper/PriceHelper.kt
+++ app/src/main/java/com/seyone22/cook/helper/PriceHelper.kt
@@ -1,21 +1,18 @@
 package com.seyone22.cook.helper
 
-import android.util.Log
+import com.seyone22.cook.data.model.Ingredient
 import com.seyone22.cook.data.model.IngredientVariant
-import com.seyone22.cook.data.model.Recipe
 import com.seyone22.cook.data.model.RecipeIngredient
 
 object PriceHelper {
     private fun priceOf(
-        variant: IngredientVariant,
-        quantity: Double
+        variant: IngredientVariant, quantity: Double
     ): Double { // Should pass quantity from scale (or default value), and price from ingredientVariety
         return (variant.price?.div(variant.quantity))?.times(quantity) ?: -1.0
     }
 
     private suspend fun getCheapestVariant(
-        ingredientId: Long?,
-        variants: List<IngredientVariant?>
+        ingredientId: Long?, variants: List<IngredientVariant?>
     ): IngredientVariant? {
         // Find the cheapest variant for the given ingredientId
         var cheapestVariant: IngredientVariant? = null
@@ -32,9 +29,7 @@ object PriceHelper {
     }
 
     suspend fun getCheapestPrice(
-        ingredientId: Long?,
-        variants: List<IngredientVariant?>,
-        quantity: Double
+        ingredientId: Long?, variants: List<IngredientVariant?>, quantity: Double
     ): Double {
         val cheapestVariant = getCheapestVariant(ingredientId, variants)
         return if (cheapestVariant != null) {
@@ -52,6 +47,27 @@ object PriceHelper {
         var totalCost: Double = 0.0
         recipeIngredients.forEach { recipeIngredient ->
             if (recipeIngredient != null) {
+                totalCost += getCheapestPrice(
+                    recipeIngredient.ingredientId,
+                    variantsList,
+                    (recipeIngredient.quantity * scaleFactor)
+                )
+            }
+        }
+        return totalCost
+    }
+
+    suspend fun getCostOfRecipeUnavailable(
+        recipeIngredients: List<RecipeIngredient?>,
+        ingredientList: List<Ingredient?>,
+        variantsList: List<IngredientVariant?>,
+        scaleFactor: Double
+    ): Double {
+        var totalCost: Double = 0.0
+        recipeIngredients.forEach { recipeIngredient ->
+            if ((recipeIngredient != null) && !ingredientList.find {
+                    (it?.id ?: -1) == recipeIngredient.ingredientId
+                }?.stocked!!) {
                 totalCost += getCheapestPrice(
                     recipeIngredient.ingredientId,
                     variantsList,
