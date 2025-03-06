fe617332eeeebd6168b234ac282c84cf23d7c008
Seyone Gunasingham <s.g.seyone@protonmail.com>
2025-03-06 10:38:00 +0530

    Change `getIngredientById` to use UUID as parameter type.

--- app/src/main/java/com/seyone22/cook/data/repository/ingredient/IngredientRepository.kt
+++ app/src/main/java/com/seyone22/cook/data/repository/ingredient/IngredientRepository.kt
@@ -2,13 +2,14 @@ package com.seyone22.cook.data.repository.ingredient
 
 import com.seyone22.cook.data.model.Ingredient
 import kotlinx.coroutines.flow.Flow
+import java.util.UUID
 
 interface IngredientRepository {
-    suspend fun insertIngredient(ingredient: Ingredient): Long
+    suspend fun insertIngredient(ingredient: Ingredient)
     suspend fun deleteIngredient(ingredient: Ingredient)
     suspend fun updateIngredient(ingredient: Ingredient)
 
-    suspend fun getIngredientById(ingredientId: Int): Flow<Ingredient?>
+    suspend fun getIngredientById(ingredientId: UUID): Flow<Ingredient?>
     suspend fun getAllIngredients(): Flow<List<Ingredient?>>
     suspend fun getIngredientByName(nameEn: String): Flow<Ingredient?>
 }
